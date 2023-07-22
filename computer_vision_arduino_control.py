bb5090e70505698b6cfd62ff03c7e168d11c1be3
mikeisadev <102952044+mikeisadev@users.noreply.github.com>
2023-07-22 21:44:16 +0200

    Update computer_vision_arduino_control.py
    Python code for computer vision and to send inputs to ARDUINO

--- computer_vision_arduino_control.py
+++ computer_vision_arduino_control.py
@@ -1,7 +1,10 @@
+# Import modules for computer vision, time and math
 import cv2
 import mediapipe as mp
 import time
 import math
+
+# Import this module to send inputs to ARDUINO
 from cvzone.SerialModule import SerialObject
 
 cap = cv2.VideoCapture(1)
@@ -19,9 +22,16 @@ index_y = 0
 thumb_x = 0
 thumb_y = 0
 
+# Max distance to get BLUE (B) color
 max_i_t_pxs = 200
+
+# Max distance to get GREEN (G) color
 middle_i_t_pxs = 120
+
+# Max distance to get RED (R) color
 min_i_t_pxs = 50
+
+# Max distance to switch off RGB LED
 switchoff = 30
 
 color = ''
@@ -34,30 +44,25 @@ while True:
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
 
-    # Loop through each HAND and get HAND LANDMARKS (x, y)
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
             for id, lm in enumerate(handLms.landmark):
-                # print(id, lm)  # Get hand point ID and landmarks (x, y)
-
-                # print(results.multi_hand_landmarks)
 
                 # Get ID position in PX
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
 
-                # print(id, cx, cy)
-
-                # Get index X/Y and thumb X/Y
+                # Get index X/Y
                 if (id == 8):
                     index_x = cx
                     index_y = cy
 
+                # Get thumb X/Y
                 if (id == 4):
                     thumb_x = cx
                     thumb_y = cy
 
-                # Draw line between Index and Thumb (hypotenus)
+                # Draw line between Index and Thumb (hypotenuse)
                 cv2.line(img, (index_x, index_y),
                          (thumb_x, thumb_y), (255, 0, 255), 2)
 
@@ -67,15 +72,18 @@ while True:
 
                 cv2.circle(img, (i_m_x, i_m_y), 4, (255, 0, 255), 4)
 
-                # Find length of hypotenuse, but first calc catheti
+                # Calc the two cathets
                 cathet1 = abs(thumb_x - index_x)
                 cathet2 = abs(index_y - thumb_y)
 
+                # Calc the hypotenuse
                 hypotenuse = math.sqrt((cathet1**2) + (cathet2 ** 2))
 
+                # Put a text to print the length of HYPOTENUSE in pixels a little above the hypotenuse line
                 cv2.putText(img, str(math.trunc(hypotenuse)),
                             (i_m_x + 20, i_m_y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 1)
 
+                # Check hypotenuse length and set colors
                 if int(hypotenuse) >= max_i_t_pxs:
                     color = "blu"
                     arduino.sendData([0, 0, 1])
